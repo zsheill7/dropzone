f176a6b85d3e7171053eb83e33c80c8bde6beee0
Zoe Sheill <zsheill7@gmail.com>
2018-07-13 11:31:25 -0700

    increased max file size

--- dist/dropzone.js
+++ dist/dropzone.js
@@ -225,7 +225,7 @@ var Dropzone = function (_Emitter) {
          * the event `maxfilesexceeded` will be called. The dropzone element gets the
          * class `dz-max-files-reached` accordingly so you can provide visual feedback.
          */
-        maxFilesize: 256,
+        maxFilesize: 2500,
 
         /**
          * The name of the file param that gets transferred.
